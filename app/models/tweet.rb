a08fa5b2a56f6edcb70e3c43334caed45be5721a
ry <zijoyrb@gamil.com>
2020-07-27 11:09:48 +0900

    モデルに記述された処理を削除しましょう

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,11 +5,4 @@ class Tweet < ApplicationRecord
   has_many :comments  # commentsテーブルとのアソシエーション
   validates :text, :image, presence: true
 
-  def self.search(search)
-    if search
-      Tweet.where('text LIKE(?)', "%#{search}%")
-    else
-      Tweet.all
-    end
-  end
 end
